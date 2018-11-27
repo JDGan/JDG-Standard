bc0cec06a13a1bc28c3e79fe696971f38ed134c5
甘邻龙01516778 <ganlinlong@haier.com>
2018-11-27 17:33:29 +0800

    增加宏

--- public/JDGMacroDefines.h
+++ public/JDGMacroDefines.h
@@ -8,5 +8,24 @@
 #ifndef JDGMacroDefines_h
 #define JDGMacroDefines_h
 
+#pragma mark - 自定义通用线程
+#define MainThread(block)                                   \
+    if ([NSThread isMainThread]) {                          \
+        block();                                            \
+    } else {                                                \
+        dispatch_async(dispatch_get_main_queue(), block);   \
+    }
+
+#define GlobalThread(block)                                 \
+    if ([NSThread isMainThread]) {                          \
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),block);\
+    } else {                                                \
+        block();                                            \
+    }
+
+#pragma mark - 自定义blocks
+typedef void(^VoidBlock)(void);
+typedef void(^DataBlock)(id data);
+typedef void(^SuccessDataBlock)(BOOL success, id data);
 
 #endif /* JDGMacroDefines_h */
